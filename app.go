3accc47078e31ad31602a60f8275ce005ed769a4
= <=>
2022-06-18 20:55:35 +0700

    successfully to get another module

--- app.go
+++ app.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	g1 "github.com/jrarkaan/golang-module"
+)
 
 func main() {
+	fmt.Println(g1.SayHello())
 	fmt.Println("raka")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jrarkaan/golang-app-ex
 
 go 1.16
+
+require github.com/jrarkaan/golang-module v1.0.0 // indirect
